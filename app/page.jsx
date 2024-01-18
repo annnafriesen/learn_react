afba8f11907878777fbab28ba47e38cdd9876dbc
annnafriesen <annafriesen@shaw.ca>
2024-01-17 20:40:24 -0800

    Created app

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,20 @@
 "use client"
 
+//add this!
+import Form from '@/components/Form';
+import Result from '@/components/Result.jsx';
+import { useState } from 'react';
+
 import { calculate } from "@/utility/helpers" // I added this function beforehand
 
+
 export default function Home() {
+  const [ score, setScore ] = useState(10);
+  
+  function calculateScore(name, major) {
+    setScore(calculate(name, major));
+  }
+
   return (
     <main style={{
       padding: 100,
@@ -15,10 +27,18 @@ export default function Home() {
         src="/images/teddy.png" 
         alt="Insanely built teddy hauling an inordinately large domino (not the pizza)"
         style={{ width: 100 }} />
-      <h1>Hello 👋</h1>
+      <h1>Rizzulator</h1>
       <p>
         We're so glad you're here :)
       </p>
+
+      {score == 0 && (
+        <Form handleCalculate={calculateScore}/> //this will always be true
+      )}
+      {score != 00 && (
+        <Result score={score}/>
+      )}
+      
     </main>
   )
 }
